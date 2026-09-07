f1dd18de6367216807804e7979250401daa13f12
ryandong8282 <ryandong82@gmail.com>
2026-09-07 15:43:37 +0800

    feat(backend): add application entry point

---
+++ backend/src/main/java/com/motolink/MotoLinkApplication.java
@@ -0,0 +1,12 @@
+package com.motolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoLinkApplication.class, args);
+    }
+}
